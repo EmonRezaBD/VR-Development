2726f86cd96e26cbdd22e4fda4f438f07a1494ce
Md Rokonuzzaman Reza <44805545+EmonRezaBD@users.noreply.github.com>
2025-01-06 10:43:54 +0600

    GLM linked and Working

--- GraphicsProj01/GraphicsProj01/GraphicsProj01.cpp
+++ GraphicsProj01/GraphicsProj01/GraphicsProj01.cpp
@@ -3,7 +3,11 @@
 
 int main()
 {
+
+    glm::vec3(0.f);
+
     //GLFWwindow* wind = glfwCreateWindow(100, 60, "Main");
+    glewExperimental = GL_TRUE;
 
     std::cout << "Hello World!\n";
 }
--- GraphicsProj01/GraphicsProj01/libs.h
+++ GraphicsProj01/GraphicsProj01/libs.h
@@ -1,4 +1,14 @@
 #pragma once
 
 #include<iostream> 
+#include<glew.h> //include glew before glfw
+
 #include<glfw3.h>
+
+#include<glm.hpp> //A math library
+#include<vec2.hpp>
+#include<vec3.hpp>
+#include<vec4.hpp>
+#include<mat4x4.hpp>
+#include<gtc\matrix_transform.hpp>
+#include<gtc\type_ptr.hpp>
